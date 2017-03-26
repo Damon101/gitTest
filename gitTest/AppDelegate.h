7be1f8f0321a8767901241305a69e5327f88ec1b
zw <zwzw_0666@qq.com>
2017-03-26 14:43:16 +0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by kirito on 2017/3/26.
+//  Copyright © 2017年 kirito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by kirito on 2017/3/26.
+//  Copyright © 2017年 kirito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
